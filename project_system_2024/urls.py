cfd1f77aaab9628e505be9f40553b9dd6401f615
Omur <1030520002@erciyes.edu.tr>
2024-03-06 10:41:26 +0300

    Starter

---
+++ project_system_2024/urls.py
@@ -0,0 +1,43 @@
+"""
+URL configuration for project_system_2024 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.contrib.auth.models import User
+from django.urls import path, include
+from rest_framework import serializers, viewsets, routers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'api/v1/users', UserViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('', include(router.urls)),
+    path('api/v1/api-auth/', include('rest_framework.urls')),
+]
