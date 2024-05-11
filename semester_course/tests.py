172e3fc594f5afa137e29aa34179f23ca316475a
Yagmurtascii <yagmur_tascii@hotmail.com>
2024-05-11 18:00:19 +0300

    27-semester-course-test

--- semester_course/tests.py
+++ semester_course/tests.py
@@ -14,7 +14,6 @@ class SemesterCourseTest(APITestCase):
         "email": "admin@test.com",
         "password": "123456",
         "identification_number": "1234567890"
-
     }
 
     student_user = {
@@ -40,27 +39,34 @@ class SemesterCourseTest(APITestCase):
         self.semester = Semester.objects.create(term="Bahar", year=2000)
         self.course = Course.objects.create(code="CS101", title="Course1")
 
-    def test_semester_course_create_admin(self):
+    def add_record(self, isLogout):
         semester_course_detail = {
             "semester": self.semester.id,
             "course": self.course.id,
-            "max_group_size": 100
+            "max_grup_size": 100,
         }
-        access = self.client.login(email="admin@test.com",
-                                   password="123456")
+        access = self.client.login(email=self.admin_user["email"], password=self.admin_user["password"])
         self.assertEqual(access, True)
         response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail)
+                                    semester_course_detail, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        if (isLogout):
+            access = self.client.logout()
+        return response
+
+    # Create
+    def test_semester_course_create_admin(self):
+        response = self.add_record(isLogout=False)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_semester_course_not_create_student(self):
         semester_course_detail = {
             "semester": self.semester.id,
             "course": self.course.id,
-            "max_group_size": 100
+            "max_grup_size": 100
         }
-        access = self.client.login(email="student@test.com",
-                                   password="123456")
+        access = self.client.login(email=self.student_user["email"],
+                                   password=self.student_user["password"])
         self.assertEqual(access,
                          True)
         response = self.client.post(self.SEMESTER_COURSE_URL,
@@ -68,143 +74,121 @@ class SemesterCourseTest(APITestCase):
         self.assertEqual(response.status_code,
                          status.HTTP_403_FORBIDDEN)
 
-    def test_semester_course_create_unauthenticated(self):
+    def test_semester_course_create_unauthorized(self):
         semester_course_detail = {
             "semester": self.semester.id,
             "course": self.course.id,
-            "max_group_size": 100
+            "max_grup_size": 100
         }
         response = self.client.post(self.SEMESTER_COURSE_URL,
                                     semester_course_detail)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    # List
     def test_semester_course_list_admin(self):
-        access = self.client.login(email="admin@test.com", password="123456")
+        access = self.client.login(email=self.admin_user["email"], password=self.admin_user["password"])
         self.assertEqual(access, True)
         response = self.client.get(self.SEMESTER_COURSE_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_semester_course_list_student(self):
-        access = self.client.login(email="student@test.com", password="123456")
+        access = self.client.login(email=self.student_user["email"],
+                                   password=self.student_user["password"])
         self.assertEqual(access, True)
         response = self.client.get(self.SEMESTER_COURSE_URL)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_semester_course_delete_admin(self):
-        semester_course_detail = {
-            "semester": self.semester.id,
-            "course": self.course.id,
-            "max_group_size": 100,
-        }
+    def test_semester_course_list_unauthorized(self):
+        response = self.client.get(self.SEMESTER_COURSE_URL)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-        access = self.client.login(email="admin@test.com",
-                                   password="123456")
-        response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail, format="json")
-        self.assertEqual(access, True)
+    # Delete
+    def test_semester_course_delete_admin(self):
+        response = self.add_record(isLogout=False)
         url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
-        print(url)
         response1 = self.client.delete(url)
         self.assertEqual(response1.status_code, status.HTTP_204_NO_CONTENT)
 
-    def test_semester_course_delete_student(self):
-        semester_course_detail = {
-            "semester": self.semester.id,
-            "course": self.course.id,
-            "max_group_size": 100,
-        }
-        access = self.client.login(email="admin@test.com",
-                                   password="123456")
-        self.assertEqual(access, True)
-        response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        access = self.client.logout()
-        access = self.client.login(email="student@test.com", password="123456")
+    def test_semester_course_delete_not_student(self):
+        response = self.add_record(isLogout=True)
+        access = self.client.login(email=self.student_user["email"],
+                                   password=self.student_user["password"])
         self.assertEqual(access, True)
         url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
         response1 = self.client.delete(url)
         self.assertEqual(response1.status_code, status.HTTP_403_FORBIDDEN)
 
+    def test_semester_course_delete_unauthorized(self):
+        response = self.add_record(isLogout=True)
+        url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
+        response1 = self.client.delete(url)
+        self.assertEqual(response1.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # Update-Patch
     def test_semester_course_update_patch_admin(self):
-        semester_course_detail = {
-            "semester": self.semester.id,
-            "course": self.course.id,
-            "max_group_size": 100,
-        }
-        access = self.client.login(email="admin@test.com",
-                                   password="123456")
-        self.assertEqual(access, True)
-        response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.add_record(isLogout=False)
         update_data = {
-            "max_group_size": 50
+            "max_grup_size": 50
         }
         url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
         response1 = self.client.patch(url, update_data, format="json")
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
 
-    def test_semester_course_update_patch_student(self):
-        semester_course_detail = {
-            "semester": self.semester.id,
-            "course": self.course.id,
-            "max_group_size": 100,
-        }
-        access = self.client.login(email="admin@test.com", password="123456")
-        self.assertEqual(access, True)
-        response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        access = self.client.logout()
-        access = self.client.login(email="student@test.com", password="123456")
+    def test_semester_course_not_update_patch_student(self):
+        response = self.add_record(isLogout=True)
+        access = self.client.login(email=self.student_user["email"],
+                                   password=self.student_user["password"])
         update_data = {
-            "max_group_size": 50
+            "max_grup_size": 50
         }
         url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
         response1 = self.client.patch(url, update_data, format="json")
         self.assertEqual(response1.status_code, status.HTTP_403_FORBIDDEN)
 
-    def test_semester_course_update_put_student(self):
-        semester_course_detail = {
-            "semester": self.semester.id,
-            "course": self.course.id,
-            "max_group_size": 100,
+    def test_semester_course_update_patch_unauthorized(self):
+        response = self.add_record(isLogout=True)
+        update_data = {
+            "max_grup_size": 50
         }
-        access = self.client.login(email="admin@test.com", password="123456")
-        self.assertEqual(access, True)
-        response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        access = self.client.logout()
-        self.assertEqual(access, None)
-        access = self.client.login(email="student@test.com", password="123456")
+        url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
+        response1 = self.client.patch(url, update_data, format="json")
+        self.assertEqual(response1.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # Update-Put
+    def test_semester_course_not_update_put_student(self):
+        response = self.add_record(isLogout=True)
+        access = self.client.login(email=self.student_user["email"],
+                                   password=self.student_user["password"])
         self.assertEqual(access, True)
         update_data = {
             "semester": self.semester.id,
             "course": self.course.id,
-            "max_group_size": 50
+            "max_grup_size": 50
         }
         url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
         response1 = self.client.put(url, update_data, format="json")
+
         self.assertEqual(response1.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_semester_course_update_put_admin(self):
-        semester_course_detail = {
-            "semester": self.semester.id,
-            "course": self.course.id,
-            "max_group_size": 100,
-        }
-        access = self.client.login(email="admin@test.com", password="123456")
-        self.assertEqual(access, True)
-        response = self.client.post(self.SEMESTER_COURSE_URL,
-                                    semester_course_detail, format="json")
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = self.add_record(isLogout=False)
         update_data = {
             "semester": self.semester.id,
             "course": self.course.id,
-            "max_group_size": 50
+            "max_grup_size": 50
         }
         url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
         response1 = self.client.put(url, update_data, format="json")
+        print(response1.data)
         self.assertEqual(response1.status_code, status.HTTP_200_OK)
+
+    def test_semester_course_update_put_unauthenticated(self):
+        response = self.add_record(isLogout=True)
+        update_data = {
+            "semester": self.semester.id,
+            "course": self.course.id,
+            "max_grup_size": 50
+        }
+        url = self.SEMESTER_COURSE_URL + f"{response.data['id']}/"
+        response1 = self.client.put(url, update_data, format="json")
+        self.assertEqual(response1.status_code, status.HTTP_401_UNAUTHORIZED)
