120d2ac79e5550dcc70badea6bbcc9f0ffa20a6e
omur <omur@erciyes.edu.tr>
2024-05-22 13:58:41 +0300

    course fix

--- course/permissions.py
+++ course/permissions.py
@@ -8,6 +8,6 @@ class IsAdminOrUserCanRead(BasePermission):
 
     def has_permission(self, request, view):
         if request.method == 'GET':
-            return bool(request.user or request.user.is_staff)
+            return bool(request.user.is_authenticated or request.user.is_staff)
         else:
-            return bool(request.user and request.user.is_staff)
+            return bool(request.user.is_authenticated and request.user.is_staff)
